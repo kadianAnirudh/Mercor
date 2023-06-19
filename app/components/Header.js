b371f6f324e06c7440545746d66ef394cebdf8ab
unknown <coderanirudh@gmail.com>
2023-06-19 16:31:38 +0530

    edit the content and put DnD

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,6 +2,8 @@
 
 import Kanban from "./Kanban.jsx";
 import UpperComponent from "./UpperComponent";
+import p5 from "../images/p5.png";
+import Image from "next/image";
 
 const Header = () => {
   return (
@@ -46,24 +48,21 @@ const Header = () => {
                 </svg>
                 <span class="sr-only">Toggle sidebar</span>
               </button>
-              <a
-                href="https://flowbite.com"
-                class="flex items-center justify-between mr-4"
-              >
-                <img
-                  src="https://mercor.io/assets/name-logo.png"
-                  class="mr-3 h-8"
-                  alt="Flowbite Logo"
-                />
+              <a href="" class="flex items-center justify-between mr-4">
+                <Image src={p5} className="mx-2" />
+                <h1 className="text-2xl font-semibold text-black">
+                  {" "}
+                  Project M
+                </h1>
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                   Flowbite
                 </span>
               </a>
-              <form action="#" method="GET" class="hidden md:block md:pl-2">
+              <form action="#" method="GET" className="hidden md:block md:pl-2">
                 <label for="topbar-search" class="sr-only">
                   Search
                 </label>
-                <div class="relative md:w-64">
+                <div class="relative md:w-[500px]">
                   <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                     <svg
                       class="w-5 h-5 text-gray-500 dark:text-gray-400"
@@ -82,7 +81,7 @@ const Header = () => {
                     type="text"
                     name="email"
                     id="topbar-search"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    class="bg-[#DBDBDB] border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     placeholder="Search"
                   />
                 </div>
